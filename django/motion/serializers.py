60d9608fff368719f9d38ed39a54dc4ca6dce2fa
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-04-21 18:39:53 +0200

    fix more motion model serializer

--- django/motion/serializers.py
+++ django/motion/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import MotionFrame, IMUData
+from .models import MotionFrame, IMUData, FSRData, AccelerometerData, InertialSensorData, MotionStatus, MotorJointData, GyrometerData
 
 
 class MotionFrameSerializer(serializers.ModelSerializer):
@@ -11,4 +11,41 @@ class MotionFrameSerializer(serializers.ModelSerializer):
 class IMUDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = IMUData
+        fields = "__all__"
+
+
+class FSRDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FSRData
+        fields = "__all__"
+
+
+class AccelerometerDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccelerometerData
+        fields = "__all__"
+
+
+class InertialSensorDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InertialSensorData
+        fields = "__all__"
+
+
+
+class MotionStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MotionStatus
+        fields = "__all__"
+
+
+class MotorJointDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MotorJointData
+        fields = "__all__"
+
+
+class GyrometerDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GyrometerData
         fields = "__all__"
