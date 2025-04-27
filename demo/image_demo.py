7c59e1de243eea1d1fac647b06ca6ced139ae7b8
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-04-27 10:46:47 +0200

    update image demo

--- demo/image_demo.py
+++ demo/image_demo.py
@@ -1,6 +1,13 @@
 from vaapi.client import Vaapi
 import os
 
+def count_bottom_image():
+    response = client.image.get_image_count(camera="BOTTOM")
+    print(response)
+
+def count_bottom_image_no_blurredness():
+    response = client.image.get_image_count(camera="BOTTOM", blurredness_value="None")
+    print(response)
 
 if __name__ == "__main__":
     client = Vaapi(
@@ -14,5 +21,7 @@ if __name__ == "__main__":
         log=155,
         camera="TOP",
     )
-    for i in response:
-        print(i)
+    print(response[0])
+
+    count_bottom_image()
+    count_bottom_image_no_blurredness()
