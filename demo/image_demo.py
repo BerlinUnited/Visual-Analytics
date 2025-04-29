083e368ab2cb9b668a5bc6ffa5b887166046a4f9
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-04-29 15:23:34 +0200

    improve frontend layout for annotation view improve some demos

--- demo/image_demo.py
+++ demo/image_demo.py
@@ -1,21 +1,11 @@
 from vaapi.client import Vaapi
 import os
 
-def count_bottom_image():
-    response = client.image.get_image_count(camera="BOTTOM")
-    print(response)
-
-def count_bottom_image_no_blurredness():
-    response = client.image.get_image_count(camera="BOTTOM", blurredness_value="None")
-    print(response)
-
-if __name__ == "__main__":
-    client = Vaapi(
-        base_url=os.environ.get("VAT_API_URL"),
-        api_key=os.environ.get("VAT_API_TOKEN"),
-    )
+def get_image_list():
     """
-    Get Top Images
+    Get Top Images from log 155 and print the first image
+    The list is not sorted. The actual image data is not part of the response. 
+    You have to get that from the returned image url
     """
     response = client.image.list(
         log=155,
@@ -23,5 +13,23 @@ if __name__ == "__main__":
     )
     print(response[0])
 
-    count_bottom_image()
-    count_bottom_image_no_blurredness()
+def print_image_stats():
+    all_top_images = client.image.get_image_count(camera="TOP")["count"]
+    all_bottom_images = client.image.get_image_count(camera="BOTTOM")["count"]
+
+    top_images_values_not_calculated = client.image.get_image_count(camera="TOP", blurredness_value="None")["count"]
+    bottom_images_values_not_calculated = client.image.get_image_count(camera="BOTTOM", blurredness_value="None")["count"]
+    
+    top_calculated_perc = 100 - (top_images_values_not_calculated / all_top_images) * 100
+    bottom_calculated_perc = 100 - (bottom_images_values_not_calculated / all_bottom_images) * 100
+    print(f"Top Images where blurredness factor is calculated: {top_calculated_perc}%")
+    print(f"Bottom Images where blurredness factor is calculated: {bottom_calculated_perc}%")
+
+
+if __name__ == "__main__":
+    client = Vaapi(
+        base_url=os.environ.get("VAT_API_URL"),
+        api_key=os.environ.get("VAT_API_TOKEN"),
+    )
+
+    print_image_stats()
