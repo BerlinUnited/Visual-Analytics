d9f29449fb87a928789bf6e3374a7a047b84e53c
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-05-05 20:20:43 +0200

    show some information about annotations in the admin panel list view

--- django/annotation/admin.py
+++ django/annotation/admin.py
@@ -6,6 +6,15 @@ from unfold.admin import ModelAdmin
 # Register your models here.
 class AnnotationAdmin(ModelAdmin):
     raw_id_fields = ("image",)
-    list_per_page = 50
+    list_display = ("get_id", "get_image_id", "get_frame_number", "class_name", "is_empty", "validated")
+
+    def get_id(self, obj):
+        return obj.id
+
+    def get_image_id(self, obj):
+        return obj.image.id
+
+    def get_frame_number(self, obj):
+        return obj.image.frame.frame_number
 
 admin.site.register(Annotation, AnnotationAdmin)
