5777e938d28fabc47fae27c46a31cece3185c9ae
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-05-10 22:16:30 +0200

    setup redis with django

--- django/core/settings.py
+++ django/core/settings.py
@@ -242,3 +242,14 @@ LOGGING = {
     },
 }
 """
+
+if not DEBUG:
+    CACHES = {
+        "default": {
+            "BACKEND": "django_redis.cache.RedisCache",
+            "LOCATION": "redis://redis-master.redis.svc.cluster.local:6379/1",
+            "OPTIONS": {
+                "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            }
+        }
+    }
--- django/image/views.py
+++ django/image/views.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -14,7 +16,9 @@ from . import models
 import time
 
 
+
 class ImageCountView(APIView):
+    @method_decorator(cache_page(60 * 60 * 2))
     def get(self, request):
         # Get filter parameters from query string
         query_params = request.query_params.copy()
