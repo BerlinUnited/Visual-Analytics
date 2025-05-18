9b0603b6da7e59b8c0b11db5f6f0f037ba92b001
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-05-18 13:42:05 +0200

    add property of frame_number to image

--- django/image/serializers.py
+++ django/image/serializers.py
@@ -3,6 +3,8 @@ from .models import NaoImage
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    frame_number = serializers.ReadOnlyField()
+
     class Meta:
         model = NaoImage
         fields = "__all__"
