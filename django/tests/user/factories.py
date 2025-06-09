c359fc468dfbdbcf2cee494b4ccc5b5766764134
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-06-09 19:12:23 +0200

    cleanup more ruff warnings

--- django/tests/user/factories.py
+++ django/tests/user/factories.py
@@ -2,13 +2,11 @@ from django.contrib.auth import get_user_model
 
 import factory
 from factory.django import DjangoModelFactory
-from rest_framework.authtoken.models import Token
-User = get_user_model()
 
 
 class UserFactory(DjangoModelFactory):
     class Meta:
-        model = User
+        model = get_user_model()
 
     username = factory.Faker('name')
     email = factory.Faker('email')
