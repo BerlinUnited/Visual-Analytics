b8b096803d4076d48110a36fe63e5db87a0fbceb
emma schlotter <emmacal@protonmail.com>
2025-06-09 15:56:49 +0200

    added factories, model and serializer tests

---
+++ django/tests/common/test_models.py
@@ -0,0 +1,152 @@
+import pytest
+from common.models import *
+from .factories import *
+pytestmark = pytest.mark.unit
+
+class TestCommonModels:
+
+    @pytest.mark.django_db
+    def test_event(self):
+        EventFactory.create(name="RC2024")
+        assert Event.objects.count() == 1
+        db_event = Event.objects.first()
+        assert str(db_event) == "RC2024"
+        assert db_event.name == "RC2024"
+
+    @pytest.mark.django_db
+    def test_game(self):
+        game = GameFactory.create(
+            team1="Team A",
+            team2="Team B",
+            half="half1",
+            start_time="2024-01-01 12:00:00+00:00"
+        )
+        assert Game.objects.count() == 1
+        db_game = Game.objects.first()
+        assert str(db_game) == "2024-01-01 12:00:00+00:00: Team A vs Team B half1"
+        assert db_game.team1 == "Team A"
+        assert db_game.team2 == "Team B"
+        assert db_game.half == "half1"
+        assert db_game.event is not None
+
+    @pytest.mark.django_db
+    def test_experiment(self):
+        experiment = ExperimentFactory.create(
+            name="test_experiment",
+            field="Field A"
+        )
+        assert Experiment.objects.count() == 1
+        db_experiment = Experiment.objects.first()
+        assert db_experiment.name == "test_experiment"
+        assert db_experiment.field == "Field A"
+        assert db_experiment.event is not None
+
+    @pytest.mark.django_db
+    def test_video_recording_with_game(self):
+        urls = {
+            'main': 'https://youtube.com/watch?v=123',
+            'alternate': 'https://youtube.com/watch?v=456'
+        }
+        recording = VideoRecordingFactory.create(urls=urls)
+        assert VideoRecording.objects.count() == 1
+        db_recording = VideoRecording.objects.first()
+        assert db_recording.urls == urls
+        assert db_recording.game is not None
+        assert db_recording.experiment is None
+
+    @pytest.mark.django_db
+    def test_video_recording_with_experiment(self):
+        urls = {
+            'main': 'https://youtube.com/watch?v=789'
+        }
+        experiment = ExperimentFactory.create()
+        recording = VideoRecordingFactory.create(
+            experiment=experiment,
+            urls=urls
+        )
+        assert VideoRecording.objects.count() == 1
+        db_recording = VideoRecording.objects.first()
+        assert db_recording.urls == urls
+        assert db_recording.game is None
+        assert db_recording.experiment == experiment
+
+    @pytest.mark.django_db
+    def test_log_creation(self):
+        log = LogFactory.create(
+            robot_version="V6",
+            player_number=3,
+            head_number=42,
+            body_serial="B12345678",
+            head_serial="H87654321",
+            representation_list=["Image", "BallModel"]
+        )
+        assert Log.objects.count() == 1
+        db_log = Log.objects.first()
+        assert str(db_log) == log.log_path
+        assert db_log.robot_version == "V6"
+        assert db_log.player_number == 3
+        assert db_log.head_number == 42
+        assert db_log.body_serial == "B12345678"
+        assert db_log.head_serial == "H87654321"
+        assert db_log.representation_list == ["Image", "BallModel"]
+        assert db_log.game is not None
+        assert db_log.experiment is None
+
+    @pytest.mark.django_db
+    def test_log_type_property(self):
+        game = GameFactory.create()
+        experiment = ExperimentFactory.create()
+        
+        game_log = LogFactory.create(game=game, experiment=None)
+        experiment_log = LogFactory.create(game=None, experiment=experiment)
+        
+        assert game_log.log_type == game
+        assert experiment_log.log_type == experiment
+
+    @pytest.mark.django_db
+    def test_log_properties(self):
+        game = GameFactory.create(
+            team1="Team A",
+            team2="Team B",
+            half="half1",
+        )
+        log = LogFactory.create(game=game)
+        
+        assert log.event_name == game.event.name
+        assert log.game_name == "Team A_vs_Team B_half1"
+
+    @pytest.mark.django_db
+    def test_log_status(self):
+        # Create a LogStatus with specific values
+        log_status = LogStatusFactory.create(
+            AudioData=5000,
+            BallModel=3000,
+            Image=2500,
+            ImageTop=2500,
+            num_motion_frames=8000
+        )
+        
+        assert LogStatus.objects.count() == 1
+        db_log_status = LogStatus.objects.first()
+        
+        # Check relationship with Log
+        assert db_log_status.log is not None
+        assert isinstance(db_log_status.log, Log)
+        
+        # Check specific values we set
+        assert db_log_status.AudioData == 5000
+        assert db_log_status.BallModel == 3000
+        assert db_log_status.Image == 2500
+        assert db_log_status.ImageTop == 2500
+        assert db_log_status.num_motion_frames == 8000
+        
+        # Verify some fields are nullable
+        log_status_minimal = LogStatusFactory.create(
+            log=LogFactory.create(),
+            AudioData=None,
+            BallModel=None
+        )
+        assert log_status_minimal.AudioData is None
+        assert log_status_minimal.BallModel is None
+
+
---
+++ django/tests/common/test_serializer.py
@@ -0,0 +1,124 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from common.serializers import *
+from .factories import *
+
+pytestmark = pytest.mark.unit
+
+class TestSerializers:
+    
+    @pytest.mark.django_db
+    def test_event_serializer(self):
+        event = EventFactory.create()
+        serializer = EventSerializer(event)
+        data = serializer.data
+        
+        assert data['name'] == event.name
+        assert data['country'] == event.country
+        assert data['timezone'] == event.timezone
+        
+        # Test deserialization
+        new_data = {
+            'name': 'New Event',
+            'country': 'Germany',
+            'timezone': 'Europe/Berlin'
+        }
+        serializer = EventSerializer(data=new_data)
+        assert serializer.is_valid()
+
+    @pytest.mark.django_db
+    def test_game_serializer(self):
+        game = GameFactory.create()
+        serializer = GameSerializer(game)
+        data = serializer.data
+        
+        assert data['team1'] == game.team1
+        assert data['team2'] == game.team2
+        assert data['half'] == game.half
+        assert data['event'] == game.event.id
+        
+        # Test deserialization
+        event = EventFactory.create()
+        new_data = {
+            'event': event.id,
+            'team1': 'Team X',
+            'team2': 'Team Y',
+            'half': 'half1'
+        }
+        serializer = GameSerializer(data=new_data)
+        assert serializer.is_valid()
+
+    @pytest.mark.django_db
+    def test_experiment_serializer(self):
+        experiment = ExperimentFactory.create()
+        serializer = ExperimentSerializer(experiment)
+        data = serializer.data
+        
+        assert data['name'] == experiment.name
+        assert data['field'] == experiment.field
+        assert data['event'] == experiment.event.id
+
+    @pytest.mark.django_db
+    def test_log_serializer_validation(self):
+        game = GameFactory.create()
+        experiment = ExperimentFactory.create()
+        
+        # Test valid data with game
+        valid_game_data = {
+            'game': game.id,
+            'robot_version': 'V6',
+            'player_number': 1
+        }
+        serializer = LogSerializer(data=valid_game_data, context={'request': type('Request', (), {'method': 'POST'})})
+        assert serializer.is_valid()
+        
+        # Test valid data with experiment
+        valid_exp_data = {
+            'experiment': experiment.id,
+            'robot_version': 'V6',
+            'player_number': 1
+        }
+        serializer = LogSerializer(data=valid_exp_data, context={'request': type('Request', (), {'method': 'POST'})})
+        assert serializer.is_valid()
+        
+        # Test invalid data with both game and experiment
+        invalid_data = {
+            'game': game.id,
+            'experiment': experiment.id,
+            'robot_version': 'V6'
+        }
+        serializer = LogSerializer(data=invalid_data, context={'request': type('Request', (), {'method': 'POST'})})
+        assert not serializer.is_valid()
+        assert 'Only one of game or experiment is allowed' in str(serializer.errors)
+
+    @pytest.mark.django_db
+    def test_log_serializer_properties(self):
+        log = LogFactory.create()
+        serializer = LogSerializer(log)
+        data = serializer.data
+        
+        assert data['event_name'] == log.event_name
+        assert data['game_name'] == log.game_name
+
+    @pytest.mark.django_db
+    def test_log_status_serializer(self):
+        log_status = LogStatusFactory.create()
+        serializer = LogStatusSerializer(log_status)
+        data = serializer.data
+        
+        assert data['log'] == log_status.log.id
+        assert data['AudioData'] == log_status.AudioData
+        assert data['BallModel'] == log_status.BallModel
+        assert data['num_motion_frames'] == log_status.num_motion_frames
+        
+        # Test deserialization
+        new_log = LogFactory.create()
+        new_data = {
+            'log': new_log.id,
+            'AudioData': 5000,
+            'BallModel': 3000,
+            'num_motion_frames': 8000
+        }
+        serializer = LogStatusSerializer(data=new_data)
+        assert serializer.is_valid()
